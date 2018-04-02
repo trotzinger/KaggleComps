f79ea435d415d767d12d85abe9f08382eb6eb726
Apophis <user.name>
2018-04-01 20:53:34 -0600

    trying to figure it out

--- NearestNeighbors.py
+++ NearestNeighbors.py
@@ -9,12 +9,22 @@ This file will be to try a nearest neighbors approach.
 import pandas as pd
 import scipy as sp
 import numpy as np
+from sklearn import neighbors
 
 def getData(path):
     return pd.read_csv(path, sep=',', decimal='.', header=0, names=['PassengerId','Survived','Pclass','Name','Sex','Age',
                                                                        'SibSp','Parch','Ticket','Fare','Cabin','Embarked'])
 if __name__=="__main__":
     data = getData("C:/projects/KaggleComps/trainData/train.csv")
+    print(data.head())
     data = data.drop('PassengerId', 1)
-    
+    binarySex = data['Sex'].apply(lambda x: 1 if x == 'male' else 0)
+    print(binarySex.values)
+    print(data.Survived)
+    X = [data['Survived'].values,binarySex.values]
+    y = [0,1]
+    trainNN = neighbors.KNeighborsClassifier(n_neighbors=2, weights='uniform', 
+                                                 algorithm='auto')
+    trainNN.fit(X,y)
+    print(trainNN.predict([[1]]))
     print ("PANDAS!")
